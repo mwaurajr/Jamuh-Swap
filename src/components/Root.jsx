81d052e5763fb42597af11367666b8e0e237be5c
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-28 14:11:03 +0300

    changing basics

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import "./Root.css";
-import bgvideo from "../assets/video.mp4";
+import bgvideo from "../assets/books.mp4";
 
 const Root = () => {
+
+
+  const handleClick = () => {
+    alert("Clicked Explore btn")
+  }
+
   return (
-    <div className="root">
-      <div className="bg-overlay"></div>
-      <video src={bgvideo} autoPlay muted loop class='bg-video'/>
-      
-      <div className="content">
-            <h1>Welcome</h1>
-            <p>To my site.</p>
+        <div className="landingpage">
+
+            <video src={bgvideo} autoPlay muted loop class="video-bg" />
+            <div className="bg-overlay"></div>
+
+            <div className="navbar">
+                <div className="menu">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+
+            <div className="home-text">
+                <h1>Welcome to BookSwap</h1>
+                <p>The love of learning, the sequestered nooks, and all the sweet serenity of books.</p>
+            </div>
+
+            <div className="home-btn" onClick={handleClick}>Explore</div>
+
         </div>
+    )
 
-      <div className="navbar"> 
-      <div className="menu">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      </div>
-
-    
-    </div>
-  );
 };
 
 export default Root;
