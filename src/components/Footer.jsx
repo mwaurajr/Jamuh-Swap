f70813cea78fc24b7e98c3a80db2242b673bfdcf
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-29 16:49:31 +0300

    added <br/>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,19 +51,19 @@ function Footer() {
           <h4>About the company</h4>
           <p>
 BookSwap is a free online platform that allows you to exchange books with other users. Our goal is to create a community of book lovers who can share their favorite books with one another and discover new books to read.
-
-How it works:
+<br/>
+How it works: <br/>
 
 1. List a book: If you have a book that you would like to swap, simply list it on BookSwap. You can browse our database to see if anyone is interested in your book, or you can wait for someone to request it.
-
+<br/>
 2. Request a book: If you see a book that you would like to read, you can request it from the user who has listed it. Once the other user approves your request, you will need to provide your shipping address.
-
+<br/>
 3. Ship the books: Both users are responsible for shipping their books to each other. We recommend using a trackable shipping method to ensure that the books are delivered safely and on time.
-
+<br/>
 4. Receive the books: Once the books have been shipped, it's just a matter of waiting for them to arrive. Once you receive your new book, you can start reading and enjoying it!
-
+<br/>
 BookSwap is a great way to meet new people and discover new books. We hope that you enjoy using our platform and that it helps you build your own personal library of books that you love. If you have any questions or feedback, please don't hesitate to contact us.
-
+<br/>
 Happy swapping!
           </p>
         </div>
