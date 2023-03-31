a6d5e400ac6242a3191c7aa0e9406b6ef405df6f
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-31 12:51:50 +0300

    created the layout of the page

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
+import HeroImage from "../components/HeroImage";
 import Navbar from "../components/Navbar";
-// import BMan from "../assets/BlackMan.jpg";
+
 
 function About() {
   return (
     <div className="about">
       <Navbar />
-      
+      <HeroImage heading='About P-Swap' text="
+      " />
       <Footer />
     </div>
   );
