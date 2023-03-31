a8408cfd79f489b9a79683654856efa0581f2869
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-31 13:22:08 +0300

    created copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import {
   FaPhone,
   FaMailBulk,
   FaSearchLocation,
-  FaGithub
+  FaGithub,
 } from "react-icons/fa";
 
 function Footer() {
@@ -51,31 +51,36 @@ function Footer() {
         <div className="right">
           <h4>About the company</h4>
           <p>
-P-Swap is a free online platform that allows you to exchange books with other users. Our goal is to create a community of book lovers who can share their favorite books with one another and discover new books to read.
-<br/>
-{/* How it works: <br/>
-
-1. List a book: If you have a book that you would like to swap, simply list it on BookSwap. You can browse our database to see if anyone is interested in your book, or you can wait for someone to request it.
-<br/>
-2. Request a book: If you see a book that you would like to read, you can request it from the user who has listed it. Once the other user approves your request, you will need to provide your shipping address.
-<br/>
-3. Ship the books: Both users are responsible for shipping their books to each other. We recommend using a trackable shipping method to ensure that the books are delivered safely and on time.
-<br/>
-4. Receive the books: Once the books have been shipped, it's just a matter of waiting for them to arrive. Once you receive your new book, you can start reading and enjoying it!
-<br/>
-BookSwap is a great way to meet new people and discover new books. We hope that you enjoy using our platform and that it helps you build your own personal library of books that you love. If you have any questions or feedback, please don't hesitate to contact us.
-<br/> */}
-Happy swapping!
+            P-Swap is a free online platform that allows you to exchange books
+            with other users. Our goal is to create a community of book lovers
+            who can share their favorite books with one another and discover new
+            books to read.
+            <br />
+            Happy swapping!
           </p>
         </div>
 
         <div className="social">
-  <a href="https://m.facebook.com/frankiline.mwaura"><FaFacebook size={30} style={{ color: "#ffffff", marginRight: "1rem" }} /></a>
-  {/* <a href="https://www.linkedin.com/"><FaLinkedin size={30} style={{ color: "#ffffff", marginRight: "1rem" }} /></a> */}
-  <a href="https://twitter.com/_Jamhuri"><FaTwitter size={30} style={{ color: "#ffffff", marginRight: "1rem" }} /></a>
-  <a href="https://github.com/mwaurajr"><FaGithub size={30} style={{ color: "#ffffff", marginRight: "1rem" }} /></a>
-</div>
-
+          <a href="https://m.facebook.com/frankiline.mwaura">
+            <FaFacebook
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          </a>
+          <a href="https://twitter.com/_Jamhuri">
+            <FaTwitter
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          </a>
+          <a href="https://github.com/mwaurajr">
+            <FaGithub
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          </a>
+          <span className="copyright">© P-Swap, 2023</span>
+        </div>
       </div>
     </div>
   );
