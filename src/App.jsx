6800e265faf3d097867a98929084508a9f503d29
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-04 20:56:30 +0300

    formatted doc with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import React from 'react';
-import { Routes, Route, } from 'react-router-dom';
-import About from './Routes/About';
-import LandingPage from './Routes/LandingPage';
-import Signup from './components/Signup';
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./Routes/About";
+import LandingPage from "./Routes/LandingPage";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <>
-<Routes>
-      
+      <Routes>
         <Route path="/" element={<LandingPage />} />
-    
 
-      
         <Route path="/about" element={<About />} />
-      
 
-      
         <Route path="/signup" element={<Signup />} />
-      
 
-      
-
-      
         <Route path="/dashboard" element={<LandingPage />} />
-      </Routes> 
+      </Routes>
     </>
   );
 }
-
 
 export default App;
