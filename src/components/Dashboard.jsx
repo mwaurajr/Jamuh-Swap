8e3bd10ee155a8ff4bdfb4660e226f19f492da23
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-06 14:03:13 +0300

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,88 @@
 import React from 'react';
-import './Dashboard.css';
-import Navbar from './Navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Message, Forum, Notifications } from '@material-ui/icons';
+import Conversations from './Conversations';
+import Messages from './Messages';
+import NotificationsList from './NotificationsList';
 
-const Dashboard = () => {
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function Dashboard() {
+  const classes = useStyles();
+
   return (
-    <>
-    <Navbar/>
-    <div className="dashboard-container">
-      <div className="sidebar">
-        <ul>
-          <li>Messages</li>
-          <li>Notifications</li>
-        </ul>
-      </div>
-      <div className="main-content">
-        <h1>Welcome to your dashboard!</h1>
-        <p>You can view your messages and notifications in the sidebar.</p>
-      </div>
+    <div className={classes.root}>
+      <Router>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerContainer}>
+            <List>
+              <Link to="/messages">
+                <ListItem button key="Messages">
+                  <ListItemIcon><Message /></ListItemIcon>
+                  <ListItemText primary="Messages" />
+                </ListItem>
+              </Link>
+              <Link to="/conversations">
+                <ListItem button key="Conversations">
+                  <ListItemIcon><Forum /></ListItemIcon>
+                  <ListItemText primary="Conversations" />
+                </ListItem>
+              </Link>
+              <Link to="/notifications">
+                <ListItem button key="Notifications">
+                  <ListItemIcon><Notifications /></ListItemIcon>
+                  <ListItemText primary="Notifications" />
+                </ListItem>
+              </Link>
+            </List>
+          </div>
+        </Drawer>
+        <main className={classes.content}>
+          <Switch>
+            <Route path="/messages">
+              <Messages />
+            </Route>
+            <Route path="/conversations">
+              <Conversations />
+            </Route>
+            <Route path="/notifications">
+              <NotificationsList />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
-    </>
-    
   );
 }
 
