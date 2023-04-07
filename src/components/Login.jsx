41b8711bd0f61cab58f8a365b5caf80ac2b4f383
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-07 20:55:08 +0300

    added Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost:3000/login', { email: email, password: password })
+      .then(response => {
+        // Handle successful login
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+        <button type="submit">Login</button>
+        <div className="alt">
+							Don't have an account?{" "}
+							<Link to="/signup" className="link">
+								Sign Up
+							</Link>
+						</div>
+      </form>
+     
+    </div>
+    </>
+  );
+}
+
+export default Login;
