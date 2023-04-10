0fed73ea0807503f3f13e8cc6a11a1ca436766ee
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-10 19:26:17 +0300

    book card made

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "./Routes/About";
 import LandingPage from "./Routes/LandingPage";
-import Signup from "./components/Signup";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Messages from "./components/Messages";
 import Conversations from "./components/Conversations";
 import Notifications from "./components/Notifications";
 
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  
+  useEffect(() => {
+    // Fetch user authorization status from backend API
+    const token = localStorage.getItem('jwt'); // or any other way to get the JWT token from local storage
+    if (token) {
+      fetch('http://localhost:3000/user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(response => response.json())
+        .then(data => setIsAuthorized(true))
+        .catch(error => console.error(error));
+    }
+  }, []);
+
+
+  useEffect(() => {
+    // Fetch user's books and reviews from backend API
+    if (isAuthorized) {
+      const token = localStorage.getItem('jwt'); // or any other way to get the JWT token from local storage
+      fetch(`http://localhost:3000/books/${user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+
+      fetch(`http://localhost:3000/reviews/${user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    }
+  }, [isAuthorized]);
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
 
         <Route path="/about" element={<About />} />
+
+        <Route path="/login" element={<Login />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
 
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,55 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./PricingCard.css";
+import React from 'react';
 
-function PricingCard() {
+const BookCard = ({ title, author, coverImg, description }) => {
   return (
-    <div className="pricing">
-      <div className="card-container">
-        <div className="card">
-          <h3>-Basic-</h3>
-          <span className="bar">
-            <p className="btc">1 BTC</p>
-            <p>3days</p>
-            <p>Views</p>
-            <p>Featured</p>
-            <p>Private Quaters</p>
-          </span>
-          <Link to="/contact" className="home-btn">
-            Book
-          </Link>
-        </div>
-
-        <div className="card">
-          <h3>-Suite-</h3>
-          <span className="bar">
-            <p className="btc">1 BTC</p>
-            <p>3days</p>
-            <p>Views</p>
-            <p>Featured</p>
-            <p>Private Quaters</p>
-          </span>
-          <Link to="/contact" className="home-btn">
-            Book
-          </Link>
-        </div>
-
-        <div className="card">
-          <h3>-Executive-</h3>
-          <span className="bar">
-            <p className="btc">1 BTC</p>
-            <p>3days</p>
-            <p>Views</p>
-            <p>Featured</p>
-            <p>Private Quaters</p>
-          </span>
-          <Link to="/contact" className="home-btn">
-            Book 
-          </Link>
-        </div>
-      </div>
+    <div className="book-card">
+      <img src={coverImg} alt={`${title} cover`} />
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <p>{description}</p>
     </div>
   );
 }
 
-export default PricingCard;
+export default BookCard;
